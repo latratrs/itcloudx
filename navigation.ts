8227c26e0af042d2e2ba69282194c6ac4ef557c4
latratrs <latraveltours@gmail.com>
2026-03-17 00:48:29 -0700

    Update footerData in navigation.ts to add new contact information and built info

---
+++ navigation.ts
@@ -0,0 +1,12 @@
+const footerData = {
+    primaryLinks: [
+        "Terms · Privacy",
+        "Contact: yaltshul@itcloudx.com",
+        "Built on Google Cloud & Firebase"
+    ],
+    secondaryLinks: [
+        // other links
+    ]
+};
+
+export default footerData;
